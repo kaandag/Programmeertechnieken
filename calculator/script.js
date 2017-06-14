dae6bc1e0e0a59e1f39411ed3d05e00f90db09eb
kaan-4A6C <510507@edu.rocmn.nl>
2017-06-14 09:17:01 +0200

    min werkt nu ook (totaal product af!)

--- calculator/script.js
+++ calculator/script.js
@@ -2,35 +2,27 @@ function calculate() {
     var string = document.getElementById('numbers').value;
     var res = string.split(",");
     var operator = document.getElementById('operation').value;
-    parseInt(res);
 
 
-    var output = 0;
-    var outputx = 1;
-    var outputD = res[0];
+    var output = parseFloat(res[0]);
 
     if (operator == "sum") {
-        for (i = 0; i < res.length; i++) {
+        for (i = 1; i < res.length; i++) {
             output+=parseFloat(res[i]);
         }
-        document.getElementById("result").innerHTML = output;
 
-    // } else if (operator == "substract") {
-    //     for (i = 0; i < res.length; i++) {
-    //         output-=parseFloat(res[i]);
-    //         document.getElementById("result").innerHTML = output;
-    //     }
+    } else if (operator == "substract") {
+        for (i = 1; i < res.length; i++) {
+            output-=parseFloat(res[i]);
+        }
     } else if (operator == "multiply") {
-        for (i = 0; i < res.length; i++) {
-            outputx*=parseFloat(res[i]);
-            document.getElementById("result").innerHTML = outputx;
+        for (i = 1; i < res.length; i++) {
+            output*=parseFloat(res[i]);
         }
     } else if (operator == "divide") {
         for (i = 1; i < res.length; i++) {
-            outputD /=parseFloat(res[i]);
-
-            document.getElementById("result").innerHTML = outputD;
-
+            output /=parseFloat(res[i]);
         }
-  }
+    }
+    document.getElementById("result").innerHTML = output;
   }
