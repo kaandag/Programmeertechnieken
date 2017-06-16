bf6049cc28b3c6219b32a32916eb8bbb1c999139
kaan-4A6C <510507@edu.rocmn.nl>
2017-06-16 09:56:45 +0200

    counts longest word

--- count words/script.js
+++ count words/script.js
@@ -3,6 +3,28 @@ function countWords() {
     var letter = text.split("");
     var word = text.split(" ");
 
-    document.getElementById('resultL').innerHTML = "aantal karakters:" + letter.length +"<br>";
+    var lgth = 0;
+    var longest;
+
+    for(var i=0; i < word.length; i++){
+        if(word[i].length > lgth){
+            var lgth = word[i].length;
+            longest = word[i];
+        }
+    }
+
+    document.getElementById('resultK').innerHTML = "aantal karakters:" +  letter.length;
     document.getElementById('resultW').innerHTML = "aantal woorden:" + word.length;
+    document.getElementById('resultL').innerHTML = "langste woord:" + longest;
 }
+
+function FirstLetterUpper(string) {
+    var text = document.getElementById('textarea').value;
+    var word = text.split(" ");
+
+    for (var i = 0; i < word.length; i++) {
+        word[i].charAt(0).toUpperCase() + word[i].slice(1);
+    }
+}
+
+document.getElementById("button").addEventListener("click", FirstLetterUpper);
